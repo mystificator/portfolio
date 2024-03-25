2ec36b5ebf659c01e5a7ada43788d2385509f24a
mystificator <vipultandon.business@gmail.com>
2024-03-25 09:08:03 +0530

    Increased the intensity of hemisphere light

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,7 +10,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={2} groundColor='black' />
+      <hemisphereLight intensity={3} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.5}
